635a053d9ad1532035f6385addc1e760877ca935
Jacob <princewith@gmail.com>
2023-08-05 20:00:46 +0800

    update
    by Jacob

--- test/LuckMoney.ts
+++ test/LuckMoney.ts
@@ -10,125 +10,66 @@ describe("luckyMoney", function () {
 
   async function deployluckyMoneyFixture() {
     const [signer, signer1] = await ethers.getSigners();
-
-    let signerBalance = await ethers.provider.getBalance(signer.address);
-    console.log(`signer 0 ${signer.address} balance: ${signerBalance}`);
-
     const creator = await ethers.deployContract("LuckyMoneyCreator", signer);
-    console.log('ethers.deployContract("LuckyMoneyCreator", signer);');
-
-    logGas(
-      "signer 0",
-      signerBalance,
-      await ethers.provider.getBalance(signer.address)
-    );
-    signerBalance = await ethers.provider.getBalance(signer.address);
-
     return { creator, signer, signer1 };
   }
 
   describe("1 participant", function () {
-    it("Should create a new luckyMoney contract", async function () {
+    it("Should create a new luckyMoney contract and do a lucky draw with a different account", async function () {
       const { creator, signer, signer1 } = await loadFixture(
         deployluckyMoneyFixture
       );
       let tx;
+      let receipt;
 
-      let signerBalance = await ethers.provider.getBalance(signer.address);
+      // stage 1
 
       tx = await signer.sendTransaction({
         to: creator,
         value: totalAmount,
       });
-      let receipt = await tx.wait();
-      console.log("signer sendTransaction");
-
-      logGas(
-        "signer 0",
-        signerBalance,
-        (await ethers.provider.getBalance(signer.address)) + totalAmount
-      );
-      logGas2("signer 0", receipt);
-
-      // {
-      //   const queryResult = await creator.query(signer.address);
-      //   console.log("queryResult: ", queryResult);
-      // }
-
-      {
-        const signerBalance = await ethers.provider.getBalance(signer.address);
-        console.log(`signer 0 ${signer.address} balance: ${signerBalance}`);
-      }
+      receipt = await tx.wait();
 
       const max_participants = 1;
       tx = await creator.create(max_participants);
       receipt = await tx.wait();
 
-      {
-        const signerBalance = await ethers.provider.getBalance(signer.address);
-        console.log(`signer 0 ${signer.address} balance: ${signerBalance}`);
-      }
-
       luckyMoneyAddress = (await creator.query(signer.address))[0];
       expect(luckyMoneyAddress).not.empty;
-      console.log("luckyMoneyAddress: ", luckyMoneyAddress);
 
       const luckyMoney = await ethers.getContractAt(
         "LuckyMoney",
         luckyMoneyAddress
       );
 
-      {
-        const participants = await luckyMoney.participants();
-        console.log("participants: ", participants);
-        expect(participants.length).to.equal(0);
-      }
-
-      {
-        const luckyMoneyBalance = await ethers.provider.getBalance(
-          luckyMoneyAddress
-        );
-        console.log(
-          `luckyMoney ${luckyMoneyAddress} balance: ${luckyMoneyBalance}`
-        );
-        expect(luckyMoneyBalance).to.equal(totalAmount);
-      }
+      let luckyMoneyBalance = await ethers.provider.getBalance(
+        luckyMoneyAddress
+      );
+      expect(luckyMoneyBalance).to.equal(totalAmount);
 
       // stage 2
-      console.warn("stage 2");
 
       const signer1Balance1 = await ethers.provider.getBalance(signer1.address);
-      console.log(`signer 1 ${signer1.address} balance: ${signer1Balance1}`);
-
-      // const luckyMoney = await ethers.getContractAt(
-      //   "LuckyMoney",
-      //   luckyMoneyAddress
-      // );
 
       tx = await luckyMoney.connect(signer1).roll();
       receipt = await tx.wait();
 
-      {
-        const participants = await luckyMoney.participants();
-        console.log("participants: ", participants);
-        expect(participants.length).to.equal(1);
-      }
+      const participants = await luckyMoney.participants();
+      expect(participants.length).to.equal(1);
 
-      const luckyMoneyBalance = await ethers.provider.getBalance(
-        luckyMoneyAddress
-      );
-      console.log(
-        `luckyMoney ${luckyMoneyAddress} balance: ${luckyMoneyBalance}`
-      );
+      luckyMoneyBalance = await ethers.provider.getBalance(luckyMoneyAddress);
 
       const signer1Balance2 = await ethers.provider.getBalance(signer1.address);
-      console.log(`signer 1 ${signer1.address} balance: ${signer1Balance2}`);
 
       expect(
-        luckyMoneyBalance +
-          (signer1Balance2 - signer1Balance1) +
-          receipt?.fee!
+        luckyMoneyBalance + (signer1Balance2 - signer1Balance1) + receipt?.fee!
       ).to.equal(totalAmount);
+
+      tx = await luckyMoney.connect(signer).refund();
+      receipt = await tx.wait();
+
+      luckyMoneyBalance = await ethers.provider.getBalance(luckyMoneyAddress);
+      expect(luckyMoneyBalance).to.equal(0);
     });
 
     // it("Should do a lucky draw with a different account", async function () {
@@ -168,11 +109,3 @@ describe("luckyMoney", function () {
     // });
   });
 });
-
-function logGas(account: string, before: bigint, after: bigint) {
-  console.log(`gas used by ${account}: ${before - after}`);
-}
-
-function logGas2(account: string, receipt: any) {
-  console.log(`gas used by ${account}: ${receipt.gasUsed}`);
-}
